e1ec8343cdd9546f4cd4e3e344565265ceebf5f6
peter fotis <pfotis@gmail.com>
2021-08-22 21:54:21 +1000

    create package.json, install mongodb and create file mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,8 @@
+// CRUD create read update delete
+
+
+const mongodb = require('mongodb')
+cosnt MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongobd://127.0.0.1:27017'
+const databaseName = 'task-manager'
