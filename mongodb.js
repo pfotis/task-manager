f86eec2edf3af4b89742673d7bab354494afd671
peter fotis <pfotis@gmail.com>
2021-08-22 22:04:20 +1000

    connect with mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,14 @@
 // CRUD create read update delete
 
-
 const mongodb = require('mongodb')
-cosnt MongoClient = mongodb.MongoClient
+const MongoClient = mongodb.MongoClient
 
-const connectionURL = 'mongobd://127.0.0.1:27017'
+const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+mongodb.MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+    console.log('Connected correctly!')
+})
