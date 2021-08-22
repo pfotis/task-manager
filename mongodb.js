3fc704089dfb52b4d385d142030643d8937f0952
peter fotis <pfotis@gmail.com>
2021-08-22 22:41:21 +1000

    use insertMany

--- mongodb.js
+++ mongodb.js
@@ -13,8 +13,47 @@ mongodb.MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, cl
     
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Peter',
-        age: 36
+    // db.collection('users').insertOne({
+    //     name: 'Peter',
+    //     age: 36
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Sophie',
+    //         age: 33
+    //     }, {
+    //         name: 'Myrofora',
+    //         age: 1
+    //     } 
+    //     ], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert documents')
+    //         }
+    //         console.log(result)
+    //     }
+    // )
+    const tasks = [{
+        description: 'git status',
+        completed: true
+    }, {
+        description: 'git add .',
+        completed: true
+    }, {
+        description: 'git commit -m "message..."',
+        completed: false
+    }]
+
+    db.collection('tasks').insertMany(tasks, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents')
+        }
+        console.log(result)
     })
 })
