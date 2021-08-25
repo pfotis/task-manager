1e450caec9ea822bab343f296904f83c66de0c9d
peter fotis <pfotis@gmail.com>
2021-08-25 20:31:02 +1000

    call mongoose and user file inside the index.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
 
 const Task = mongoose.model('Task', {
     description: {
@@ -17,27 +15,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-})
-
-// const me = new User({
-//    name: '   Myrofora    ',
-//    email: '   SOPHIE@example.COM.AU      ',
-//    password: '    password  '
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-const task = new Task({
-    description: '      clean the house    ',
-    completed: 'true'
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
 })
