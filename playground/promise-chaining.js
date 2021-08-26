3099e1c57e7f3c59abfe5a4afc0a04bdf78f14cf
peter fotis <pfotis@gmail.com>
2021-08-26 17:38:07 +1000

    update the mongoose.js file

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,7 +1,7 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('61260447864deb053276d178', { age: 36 }).then((user => {
+User.findByIdAndUpdate('612607d7b72f0605f806e271', { age: 36 }).then((user => {
     console.log(user)
     return User.countDocuments({ age: 36 })
 })).then((result) => {
