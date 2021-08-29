f9737d4b235c2064349ec1e6b56ebe7e592faa43
peter fotis <pfotis@gmail.com>
2021-08-29 21:09:11 +1000

    update the promise-chaining-2.js file

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findOneAndDelete('61260b7c91589e071591b4d1').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findOneAndDelete('61260b7c91589e071591b4d1').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const deleteTaskAndCount = async (id, { completed }) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed })
+    return count
+}
+
+deleteTaskAndCount('1260b7c91589e071591b', false).then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
