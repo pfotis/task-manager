f47bfb37524211cbff206e5855520534e58cd88f
peter fotis <pfotis@gmail.com>
2021-09-01 22:17:07 +1000

    http request patch for task

--- src/index.js
+++ src/index.js
@@ -42,8 +42,16 @@ app.get('/users/:id', async (req, res) => {
 })
 
 app.patch('/users/:id',  async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
     try {
-        const user =  await User.findByIdAndUpdate(req.paraams.id, req.body, { new: true, runValidators: true })
+        const user =  await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         if (!user) {
             return res.status(404).send()
         }
@@ -55,7 +63,7 @@ app.patch('/users/:id',  async (req, res) => {
 
 app.get('/tasks', async (req, res) => {
     try {
-        const tacks = await Task.find({})
+        const tasks = await Task.find({})
         res.send(tasks)
     } catch (e) {
         res.status(500).send()
@@ -75,13 +83,31 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
-
-
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try {
         await task.save()
         res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed', 'description']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(400).send()
+        }
+        res.status(200).send(task)
     } catch (e) {
         res.status(400).send(e)
     }
