dbc092e8822ee43977575e52882355586a28f2b4
peter fotis <pfotis@gmail.com>
2021-09-01 21:36:34 +1000

    create a new http request patch

--- src/index.js
+++ src/index.js
@@ -41,6 +41,18 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id',  async (req, res) => {
+    try {
+        const user =  await User.findByIdAndUpdate(req.paraams.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.get('/tasks', async (req, res) => {
     try {
         const tacks = await Task.find({})
@@ -62,6 +74,8 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+
+
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
