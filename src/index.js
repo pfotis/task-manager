7da305937c9ebec5b717f7dd49ec36a18b4948a2
peter fotis <pfotis@gmail.com>
2021-09-01 23:01:46 +1000

    http request delete task

--- src/index.js
+++ src/index.js
@@ -125,6 +125,18 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
