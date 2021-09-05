c95dc44ea57ccbe6612e463c0aa5dc17c1b5dd1a
peter fotis <pfotis@gmail.com>
2021-09-05 22:15:52 +1000

    install bcryprjs and update the files src/user.js and models/user.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
