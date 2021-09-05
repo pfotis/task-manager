3fde92f9eab1c529c6e16c1354fe874d30143388
peter fotis <pfotis@gmail.com>
2021-09-05 21:38:54 +1000

    update the file src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
@@ -10,7 +7,6 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
 app.use(userRouter)
 app.use(taskRouter)
 
