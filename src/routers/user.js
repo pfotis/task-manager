22e92d49845f4ed19093f89f7b3e9b4240f04190
peter fotis <pfotis@gmail.com>
2021-09-09 22:19:25 +1000

    change the http request get /users to get /users/me

--- src/routers/user.js
+++ src/routers/user.js
@@ -25,13 +25,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', auth, async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
