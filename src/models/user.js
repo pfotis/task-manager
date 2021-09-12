199a5eb14ba78d19c715fa38ff4039f30205e754
peter fotis <pfotis@gmail.com>
2021-09-12 19:46:02 +1000

    change name the method getPublicProfile to toJSON

--- src/models/user.js
+++ src/models/user.js
@@ -50,9 +50,12 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = async function() {
+userSchema.methods.toJSON = function() {
     const user = this
     const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
 
     return userObject
 }
