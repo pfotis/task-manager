c157e48295a7181b146f080caf5d33d656687b2e
peter fotis <pfotis@gmail.com>
2021-09-12 20:22:23 +1000

    updated http request patch /users/:id to /users/me

--- src/routers/user.js
+++ src/routers/user.js
@@ -54,7 +54,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.patch('/users/:id',  async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => {
@@ -64,18 +64,13 @@ router.patch('/users/:id',  async (req, res) => {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
     try {
-        const user = await User.findById(req.params.id)
+        const user = await User.findById(req.user._id)
 
         updates.forEach((update) => {
             user[update] = req.body[update]
         })
-
         await user.save()
 
-        // const user =  await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send()
-        }
         res.send(user)
     } catch (e) {
         res.status(400).send(e)
